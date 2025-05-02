0d7361ec63ea9ab20ead0e97567658e13ddf98ff
Prakhand Patel <107527663+Shivaay19@users.noreply.github.com>
2025-05-02 21:17:09 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4IBH8mBI6iTx_QxGNnf7VKLbgh3dVa4I',
-  authDomain: 'newsarchive-41ec9.firebaseapp.com',
-  projectId: 'newsarchive-41ec9',
-  storageBucket: 'newsarchive-41ec9.appspot.com',
-  messagingSenderId: '635802447680',
-  appId: '1:635802447680:web:9f6f54cd0ca8d31bbed4a5',
-  measurementId: 'G-QLYGS91LEQ'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 const app = initializeApp(firebaseConfig);
